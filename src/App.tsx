38f42d525a08cb446368626c3fa2998c975af99c
jongsu <hjs1002@jei.com>
2024-02-05 12:51:35 +0900

    3d dice

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Dice } from './components/diceBox.js';
+import './App.css';
+
+Dice.init().then(() => {
+    console.log('load');
+    Dice.show().roll(['2d20', '1d6']);
+    // clear dice on click anywhere on the screen
+    document.addEventListener('mousedown', () => {
+        const diceBoxCanvas = document.getElementById('dice-canvas');
+        if (diceBoxCanvas && diceBoxCanvas.getAttribute('display') !== 'none') {
+            Dice.hide().clear();
+        }
+    });
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+    const onRoll = () => {
+        // 2d20 : 주사위 2개, 면 20개
+        // 1d6 : 주사위 1개, 면 6개
+        Dice.show().roll(['2d20', '1d6']);
+    };
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    Dice.onRollComplete = (results: any) => {
+        console.log(results);
+    };
+
+    return (
+        <div className='app'>
+            <h1>3d 주사위 굴리기</h1>
+            <button onClick={onRoll}>roll dice</button>
+        </div>
+    );
 }
 
-export default App
+export default App;
